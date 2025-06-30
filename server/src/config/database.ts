ae0f660ae0a4f18656718633cff77b9d0845226d
Jinyehee <ounnms@gmail.com>
2025-06-30 16:42:52 +0900

    db 연결

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,6 +1,19 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-export const sequelize = new Sequelize("gpsapp", "gpsuser", "비밀번호", {
-  host: "localhost",
-  dialect: "mysql",
-});
+dotenv.config();
+
+export const sequelize = new Sequelize(
+  process.env.DB_NAME as string,
+  process.env.DB_USER as string,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    dialect: "mysql",
+    logging: false,
+    define: {
+      timestamps: false,
+    },
+  }
+);
--- server/src/index.ts
+++ server/src/index.ts
@@ -9,8 +9,18 @@ app.use(express.json());
 
 app.use("/api/users", userRoutes);
 
-sequelize.sync().then(() => {
-  app.listen(5000, () => {
-    console.log("✅ Server running on http://localhost:5000");
+// 연결 체크 후 서버 시작
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("✅ DB 연결 성공!");
+    return sequelize.sync();
+  })
+  .then(() => {
+    app.listen(5000, () => {
+      console.log("🚀 Server running on http://localhost:5000");
+    });
+  })
+  .catch((error) => {
+    console.error("❌ DB 연결 실패:", error);
   });
-});
