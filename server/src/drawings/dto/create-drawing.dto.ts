3f782e3019789b9f3770db4565194b553ce1190b
Jinyehee <ounnms@gmail.com>
2025-07-02 16:21:30 +0900

    drawing테이블제거

--- server/src/drawings/dto/create-drawing.dto.ts
+++
@@ -1,6 +0,0 @@
-export class CreateDrawingDto {
-  userId: string;
-  drawingTitle: string;
-  drawingDescription?: string;
-  drawingThumbnail?: string; // 선택사항이면 ? 붙이기
-}
--- server/src/drawings/dto/update-drawing.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateDrawingDto } from './create-drawing.dto';
-
-export class UpdateDrawingDto extends PartialType(CreateDrawingDto) {
-  userId?: string;
-  drawingTitle?: string;
-  drawingDescription?: string;
-  drawingThumbnail?: string; // 선택사항이면 ? 붙이기
-}
