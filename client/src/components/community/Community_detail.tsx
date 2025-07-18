6c6a1cb0cc23743fd9e7aedb578b06f64a4e26cc
nakyoung1 <skrud5730@naver.com>
2025-07-18 16:18:24 +0900

    게시글 상세내용 프로필 수정

--- client/src/components/community/Community_detail.tsx
+++ client/src/components/community/Community_detail.tsx
@@ -1,6 +1,7 @@
 import { useParams, useNavigate } from "react-router-dom";
 import "../css/Community_detail.css";
 import sampleImage from "../../assets/ex2.jpg"; // 예시 이미지 경로
+import profile from "../../assets/profile.png";
 
 const Community_detail = () => {
   const { id } = useParams(); // 게시물 ID
@@ -22,11 +23,14 @@ const Community_detail = () => {
       {/* 본문 영역 */}
       <main className="detail-content">
         <div className="detail-writer">
-          <p className="writer-name">유저 닉네임</p>
-          <p className="writer-meta">올린 시간</p>
+          <div className="writer-profile">
+            <img src={profile} alt="작성자 프로필" className="writer-img" />
+            <p className="writer-name">푸른달걀</p>
+          </div>
+          <p className="writer-meta">3초전</p>
         </div>
-        <h2 className="detail-title">게시글 제목</h2>
-        <p className="detail-text">게시글 내용</p>
+        <h2 className="detail-title">산책가기 싫은 댕댕이</h2>
+        <p className="detail-text">*내용*산책가기 싫은 댕댕이</p>
         <img src={sampleImage} className="detail-image" alt="sample" />
 
         <div className="detail-stats">👁 2명이 봤어요</div>
@@ -49,7 +53,6 @@ const Community_detail = () => {
       {/* 하단 댓글 입력창 */}
       <footer className="detail-footer">
         <input type="text" placeholder="댓글을 입력해주세요." />
-        <button>😊</button>
       </footer>
     </div>
   );
