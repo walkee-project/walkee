b6bcb32f2b7ec984f719e21cea59440ff64d8f26
Jinyehee <ounnms@gmail.com>
2025-07-18 10:27:23 +0900

    오류 수정!

--- client/src/global.d.ts
+++ client/src/global.d.ts
@@ -4,7 +4,7 @@ export {};
 
 declare global {
   interface Window {
-    kakao: any;
+    kakao: typeof kakao;
   }
 
   namespace kakao {
@@ -15,22 +15,87 @@ declare global {
         getLng(): number;
       }
 
+      interface MapOptions {
+        center: LatLng;
+        level?: number;
+        draggable?: boolean;
+        scrollwheel?: boolean;
+        disableDoubleClick?: boolean;
+        disableDoubleClickZoom?: boolean;
+        tileAnimation?: boolean;
+        projectionId?: number;
+      }
+
       class Map {
-        constructor(container: HTMLElement, options: any);
+        constructor(container: HTMLElement, options: MapOptions);
         setCenter(latLng: LatLng): void;
+        getCenter(): LatLng;
+        getLevel(): number;
+      }
+
+      interface MarkerOptions {
+        map?: Map;
+        position: LatLng;
+        image?: MarkerImage;
+        title?: string;
+        clickable?: boolean;
+        draggable?: boolean;
+        zIndex?: number;
       }
 
       class Marker {
-        constructor(options: any);
+        constructor(options: MarkerOptions);
         setPosition(latLng: LatLng): void;
         getPosition(): LatLng;
         getMap(): Map | null;
+        setMap(map: Map | null): void;
+      }
+
+      interface MarkerImage {
+        size: Size;
+        options?: MarkerImageOptions;
+      }
+
+      class Size {
+        constructor(width: number, height: number);
+        getWidth(): number;
+        getHeight(): number;
+      }
+
+      interface MarkerImageOptions {
+        offset?: Point;
+        spriteOrigin?: Point;
+        spriteSize?: Size;
+        alt?: string;
+        shape?: string;
+      }
+
+      class Point {
+        constructor(x: number, y: number);
+        getX(): number;
+        getY(): number;
+      }
+
+      interface PolylineOptions {
+        map?: Map;
+        path: LatLng[];
+        strokeWeight?: number;
+        strokeColor?: string;
+        strokeOpacity?: number;
+        strokeStyle?:
+          | "solid"
+          | "shortdash"
+          | "shortdot"
+          | "longdash"
+          | "dashdot"
+          | "longdashdot";
       }
 
       class Polyline {
-        constructor(options: any);
+        constructor(options: PolylineOptions);
         setMap(map: Map | null): void;
         setPath(path: LatLng[]): void;
+        getPath(): LatLng[];
       }
 
       namespace services {
@@ -56,8 +121,16 @@ declare global {
       namespace drawing {
         type OverlayType = "POLYLINE" | "MARKER";
 
+        interface DrawingManagerOptions {
+          map: Map;
+          drawingMode?: OverlayType[];
+          guideTooltip?: ["draw", "drag", "edit"];
+          markerOptions?: MarkerOptions;
+          polylineOptions?: PolylineOptions;
+        }
+
         class DrawingManager {
-          constructor(options: any);
+          constructor(options: DrawingManagerOptions);
           select(type: OverlayType): void;
         }
       }
