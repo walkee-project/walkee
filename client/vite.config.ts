e86e514563297518010b01a0363c2ae50ed5d7be
nakyoung1 <skrud5730@naver.com>
2025-07-22 15:37:32 +0900

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   server: {
     host: true, // 외부 IP에서도 접근 가능하게
