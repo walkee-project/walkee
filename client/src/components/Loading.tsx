53b0bebbddc0d1332049f4b9053773abcb7c666e
yoenheep <yoen3212@naver.com>
2025-07-25 15:46:56 +0900

    로딩배경수정

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -1,11 +1,12 @@
 import "./css/Loading.css";
 import mascot from "../assets/mascot_long.png";
+import load_bg from "../assets/load_bg.png";
 
 export default function Loading() {
   return (
     <div className="loading">
       <p>Loading...</p>
-      <div className="load_box">
+      <div className="load_box" style={{ backgroundImage: `url(${load_bg})` }}>
         <div className="bar_container">
           <div className="bar_bg" />
           <img src={mascot} alt="마스코트" className="bar_mascot" />
